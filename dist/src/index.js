e2e0fe6af64beba6a40fe0967aa2a84a3d9b769d
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-07-23 23:13:35 +0700

    build: Build code to ready deploy in server

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,10 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const yamljs_1 = __importDefault(require("yamljs"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
 require("./config/database");
+const openAPIDocument = yamljs_1.default.load('./docs/openapi.yaml');
 const port = Number(process.env.PORT) || 8000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -15,6 +18,7 @@ app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, morgan_1.default)('combined'));
 app.use((0, cors_1.default)());
 app.use('/api/v1.0', routes_1.default);
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(openAPIDocument));
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
--- dist/src/routes/index.js
+++ dist/src/routes/index.js
@@ -9,7 +9,7 @@ const qrController_1 = require("../controllers/qrController");
 const responseHelper_1 = require("../utils/responseHelper");
 const router = express_1.default.Router();
 router.get('/qr/qr-generate', authenticateMiddleware_1.authentication, qrController_1.generateQris);
-router.get('/qr/qr-pay', authenticateMiddleware_1.authentication, qrController_1.generateQrisPay);
+router.post('/qr/qr-pay', authenticateMiddleware_1.authentication, qrController_1.generateQrisPay);
 router.use((req, res) => {
     (0, responseHelper_1.handleNotFound)(res, "Route not found");
 });
