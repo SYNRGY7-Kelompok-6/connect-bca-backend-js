eae4db1c0a5531724eb2d927db1b7629bf96cfba
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-07-27 23:30:43 +0700

    feature: record request and error in logs

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,4 +1,5 @@
 import { createLogger, format, transports } from 'winston';
+import { getDate } from '../utils/getDate';
 
 const { 
   combine, 
@@ -8,6 +9,8 @@ const {
   colorize 
 } = format;
 
+const dateFormat = getDate();
+
 const logger = createLogger({
   level: 'info',
   format: combine(
@@ -15,8 +18,8 @@ const logger = createLogger({
     json()
   ),
   transports: [
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' }),
+    new transports.File({ filename: `logs/error-${dateFormat}.log`, level: 'error' }),
+    new transports.File({ filename: `logs/combined-${dateFormat}.log` }),
   ],
 });
 
